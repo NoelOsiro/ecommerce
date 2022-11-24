c4671ccc9b536230f15fbf14f7313b12b5d5cad5
Noel Otieno Osiro <noel.osiro@azubiafrica.org>
2022-11-24 12:42:06 +0300

    Started Afresh

--- product-page/src/App.tsx
+++ product-page/src/App.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import './App.css';
-import MainNav from './Components/Navbar/MainNav';
-import Pdescr from './Components/PDescription/Pdescr';
-import PImage from './Components/PImage/PImage';
+
 
 function App() {
   return (
-    <div className='App'>
-      <MainNav />
-      <main>
-      <PImage/>
-      <Pdescr/>
-      </main>
-      
+    <div className='App'> 
     </div>
   );
 }
